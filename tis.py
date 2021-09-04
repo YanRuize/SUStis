d49a644b588979611ff4d51bf1d7030c0e023378
Chris <hezean@vip.qq.com>
2021-09-04 16:44:34 +0800

    Supports electing several courses at once

--- tis.py
+++ tis.py
@@ -15,13 +15,45 @@ headers = {
     '_eventId': 'submit',
     'geolocation': ''
 }
+
 ele_head = {
     "p_pylx": "1",
     "mxpylx": "1",
     "p_sfgldjr": "0",
     "p_sfredis": "0",
     "p_sfsyxkgwc": "0",
+    "p_xktjz": "rwtjzyx"
 }
+
+
+def login():
+    tis = session.post(cas_url, headers)
+    if str(tis.content, 'utf-8').startswith('<!DOCTYPE html><html>'):
+        raise Exception('Username or password incorrect')
+    print('Successfully logged in')
+
+
+def qk(course: dict):
+    ele_head.update(course)
+
+    tis = session.post(ele_url, ele_head)
+    trial_cnt = 1
+    try:
+        while tis.json()['jg'] == '-1' and trial_cnt <= 600:
+            tis = session.post(ele_url, ele_head)
+            print(trial_cnt, tis.json()['message'], sep='\t')
+            trial_cnt += 1
+            time.sleep(0.2)
+    except:
+        print(tis.content)
+    
+    try:
+        print(
+            f'\033[0;33;40m{course.get("course_name")} finished, please check manually in tis\033[0m\n', )
+    except:
+        print(
+            f'\033[0;33;40m{course} finished, please check manually in tis\033[0m\n')
+
 
 if __name__ == '__main__':
     with open('user.json') as f:
@@ -29,20 +61,11 @@ if __name__ == '__main__':
         headers.update({'username': info.get('sid'),
                         'password': info.get('pwd')})
         ele_head.update(info.get('ele_head'))
+        courses = info.get('courses')
 
-    tis = session.post(cas_url, headers)
-    if str(tis.content, 'utf-8').startswith('<!DOCTYPE html><html>'):
-        raise Exception('Username or password incorrect')
-    print('Successfully logged in')
+    login()
+    for course in courses:
+        qk(course)
 
-    tis = session.post(ele_url, ele_head)
-    trial_cnt = 1
-
-    while tis.json()['jg'] == '-1' and trial_cnt < 6000:
-        tis = session.post(ele_url, ele_head)
-        print(trial_cnt, tis.json()['message'], sep='\t')
-        trial_cnt += 1
-        time.sleep(0.2)
-
-    print(f'\n\nFinished!')
+    print(f'\nAll done, have fun!')
     session.close()
